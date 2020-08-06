3d21d849d7d07fc7549a5b252ab34085f274f3e1
tdrvlad <45488144+tdrvlad@users.noreply.github.com>
2020-08-06 17:52:40 +0300

    Compiled (Stop commadn to be implemented)

--- Main_Control.py
+++ Main_Control.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from apscheduler.schedulers.background import BackgroundScheduler
 import fbchat
 import sys
@@ -69,7 +69,7 @@ def log_event(string):
 	current_time =  datetime.now().strftime('%H:%M:%S')
 
 	f = open(log_file,'a')
-	f.write(string + ' at ' + current_time)
+	f.write(string + ' at ' + current_time + '\n')
 	f.close()
 
 def setup_gpio():
@@ -85,13 +85,13 @@ def setup_gpio():
 
 	log_event('Initialized')
 
-def stop_all(thread):
+def stop_all(thread = None):
 	GPIO.output(relay_lights,GPIO.HIGH)
 	GPIO.output(relay_power,GPIO.HIGH)
 	GPIO.output(relay_pump,GPIO.HIGH)
 	GPIO.output(relay_christmas_lights,GPIO.HIGH)
 
-def lights_on(thread):
+def lights_on(thread = None):
 	GPIO.output(relay_power,GPIO.LOW)
 	GPIO.output(relay_lights,GPIO.LOW)
 	
@@ -100,7 +100,7 @@ def lights_on(thread):
 	if thread != None:
 		thread.send_text('Lights turned on')
 
-def lights_off(thread):
+def lights_off(thread = None):
 	GPIO.output(relay_power,GPIO.HIGH)
 	GPIO.output(relay_lights,GPIO.HIGH)
 	
@@ -109,7 +109,7 @@ def lights_off(thread):
 	if thread != None:
 		thread.send_text('Lights turned on')
 
-def christmas_lights_on(thread)
+def christmas_lights_on(thread = None):
 	GPIO.output(relay_christmas_lights,GPIO.LOW)
 	GPIO.output(relay_power,GPIO.LOW)
 
@@ -118,7 +118,7 @@ def christmas_lights_on(thread)
 	if thread != None:
 		thread.send_text('Christmas lights turned on')
 
-def christmas_lights_off(thread)
+def christmas_lights_off(thread = None):
 	GPIO.output(relay_christmas_lights,GPIO.HIGH)
 	GPIO.output(relay_power,GPIO.HIGH)
 
@@ -127,7 +127,7 @@ def christmas_lights_off(thread)
 	if thread != None:
 		thread.send_text('Christmas lights turned off')
 
-def irigation_on(thread)
+def irigation_on(thread = None):
 	GPIO.output(relay_pump,GPIO.LOW)
 	GPIO.output(relay_power,GPIO.LOW)
 
@@ -136,7 +136,7 @@ def irigation_on(thread)
 	if thread != None:
 		thread.send_text('Irigation turned on')
 
-def irigation_off(thread)
+def irigation_off(thread = None):
 	GPIO.output(relay_pump,GPIO.HIGH)
 	GPIO.output(relay_power,GPIO.HIGH)
 
@@ -151,56 +151,59 @@ def irigation_off(thread)
 nom_scheduler = BackgroundScheduler()
 
 def compute_stop(start_h, start_m, dur_h, dur_m):
-	stop_h = start_h + dur_h + (start_m + dur_m) / 60
-	stop_m = (start_m + dur_m) % 60
+	stop_h = int(start_h + dur_h + (start_m + dur_m) / 60)
+	stop_m = int((start_m + dur_m) % 60)
 
 	return stop_h, stop_m
 
 
 #Summer lights
-nom_scheduler.add_cron_job(lights_on, month = '4,5,6,7,8,9', hour = lights_sum_h, minute = lights_sum_m)
+nom_scheduler.add_job(lights_on, 'cron', month = '4,5,6,7,8,9', hour = lights_sum_h, minute = lights_sum_m)
 
 lights_sum_stop_h, lights_sum_stop_m = compute_stop(lights_sum_h, lights_sum_m, lights_dur_h, lights_dur_m)
 
-nom.scheduler.add_cron_job(lights_off, month = '4,5,6,7,8,9', hour = lights_sum_stop_h, minute = lights_sum_stop_m)
+nom_scheduler.add_job(lights_off, 'cron', month = '4,5,6,7,8,9', hour = lights_sum_stop_h, minute = lights_sum_stop_m)
 
 
 #Winter lights
-nom_scheduler.add_cron_job(lights_on, month = '1,2,3,10,11,12', hour = lights_win_h, minute = lights_win_m)
+nom_scheduler.add_job(lights_on, 'cron', month = '1,2,3,10,11,12', hour = lights_win_h, minute = lights_win_m)
 
 lights_win_stop_h, lights_win_stop_m = compute_stop(lights_win_h, lights_win_m, lights_dur_h, lights_dur_m)
 
-nom.scheduler.add_cron_job(lights_off, month = '1,2,3,10,11,12', hour = lights_win_stop_h, minute = lights_win_stop_m)
+nom_scheduler.add_job(lights_off, 'cron', month = '1,2,3,10,11,12', hour = lights_win_stop_h, minute = lights_win_stop_m)
 
 
 #Irigation
-nom_scheduler.add_cron_job(irigation_on, month = '4,5,6,7,8,9', hour = irigation_h, minute = irigation_m)
+nom_scheduler.add_job(irigation_on, 'cron', month = '4,5,6,7,8,9', hour = irigation_h, minute = irigation_m)
 
-irigation_stop_h, irigation_stop_m = compute_stop(irigation_h, irigation_m, irigation_dur_h, irigation_dur_m)
+irigation_stop_h, irigation_stop_m = compute_stop(irigation_h, irigation_m, 0, irigation_dur_m)
 
-nom_scheduler.add_cron_job(irigation_off, month = '4,5,6,7,8,9', hour = irigation_stop_h, minute = irigation_stop_m)
+nom_scheduler.add_job(irigation_off, 'cron', month = '4,5,6,7,8,9', hour = irigation_stop_h, minute = irigation_stop_m)
 
 
 #Christmas Lights
-nom_scheduler.add_cron_job(christmas_lights_on, month = '1,12', hour = lights_win_h, minute = lights_win_m)
+nom_scheduler.add_job(christmas_lights_on, 'cron', month = '1,12', hour = lights_win_h, minute = lights_win_m)
 
 lights_win_stop_h, lights_win_stop_m = compute_stop(lights_win_h, lights_win_m, lights_dur_h, lights_dur_m)
 
-nom.scheduler.add_cron_job(lights_off, month = '1,2,3,10,11,12', hour = lights_win_stop_h, minute = lights_win_stop_m)
+nom_scheduler.add_job(lights_off, 'cron', month = '1,2,3,10,11,12', hour = lights_win_stop_h, minute = lights_win_stop_m)
 
 
 #Hourly Lights
 for hour in range(0,6):
-	nom_scheduler.add_cron_job(christmas_lights_on, hour = hour, minute = 0)
-	nom_scheduler.add_cron_job(christmas_lights_on, hour = hour, minute = lights_hourly_dur_m)
+	nom_scheduler.add_job(christmas_lights_on, 'cron', hour = hour, minute = 0)
+	nom_scheduler.add_job(christmas_lights_on, 'cron', hour = hour, minute = lights_hourly_dur_m)
 
 nom_scheduler.print_jobs()
-nom.scheduler.start()
+nom_scheduler.start()
 
 
 #------------ Remote Commands ------------ 
 
+setup_gpio()
+
 ev_scheduler = BackgroundScheduler()
+ev_scheduler.start()
 
 session = fbchat.Session.login(email, password)
 listener = fbchat.Listener(session=session, chat_on=False, foreground=False)
@@ -243,13 +246,13 @@ for event in listener.listen():
 				
 				#compute command
 				message = message.lower()
-				print('Received command: ', message)
-
 				split_message = message.split()
+				
+				print('Received command: ', split_message)
 
 				command = -1
 				for key in commands:
-					if all(item in split_message for item in key.split()):
+					if all(item in split_message for item in key.lower().split()):
 						command = commands.get(key)
 						comm_key = key
 
@@ -298,28 +301,26 @@ for event in listener.listen():
 					thread.send_text(str(dev_key) + ' to ' + str(comm_key) + ' in ' + str(start_delay) + ' seconds for ' + str(run_time) + ' seconds.')
 
 					current_time =  datetime.now()
-					start_time = current_time + datetime.timedelta(0, start_delay * 60)
-					stop_time = current_time + datetime.timedelta(0, (start_delay + run_time) *60 )
+					start_time = current_time + timedelta(0, start_delay * 60)
+					stop_time = current_time + timedelta(0, (start_delay + run_time) *60 )
 					
-					ev_scheduler.add_date_job(turn_on, start_time, args = (thread,))
-					ev.scheduler.add_date_job(turn_off, stops_time, args = (thread,))
-
-					ev_scheduler.start()
+					ev_scheduler.add_job(lights_on, 'date', run_date = start_time, args = (thread,))
+					ev_scheduler.add_job(lights_off, 'date', run_date = stop_time, args = (thread,))
 
 
-				if command == 4:
+				if command == 0:
 					print('Delete all scheduled jobs')
 					for job in ev_scheduler.get_jobs():
 						job.remove()
 					thread.send_text('Deleted all scheduled tasks.')
 
 
-				if command == 3:
+				if command == 2:
 					print('Reporting Log file')
 
 					try:
 						with open(log_file) as log:
-							for line in (log.readlines() [-N:]):
+							for line in (log.readlines() [-20:]):
 								thread.send_text(line, end = '')
 
 					except:
